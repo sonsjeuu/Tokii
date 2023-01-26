1d293a38ee539ee1a39171d6e70d3f01402b8838
TranXuanSon <tx.sonnn207@gmail.com>
2023-01-27 01:20:03 +0700

    A

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-unzip methods.zip; unzip ATLAS-BYPASS.zip; unzip ATLAS-METHODS.zip; mv ATLAS-METHODS methods; mv ATLAS-BYPASS methods; mv proxies.txt ua.txt methods; chmod 777 methods/*sudo apt -y update; sudo apt -y install screen git nodejs
+unzip methods.zip; unzip ATLAS-METHODS.zip; mv ATLAS-METHODS methods; mv proxies.txt ua.txt methods; chmod 777 methods/*
+sudo apt -y update; sudo apt -y install screen git nodejs
 npm i fake-useragent
 npm i randomstring
 npm i request
